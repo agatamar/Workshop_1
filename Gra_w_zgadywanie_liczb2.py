abcde7cb0bdffa20bea6e3ca5d3a6f40a32c3d56
Agata Marchewka-Madejska <agata.marchewka@gmail.com>
2018-10-13 16:12:52 +0200

    Gra w zgadywanie liczb2 corrected

--- Gra_w_zgadywanie_liczb2.py
+++ Gra_w_zgadywanie_liczb2.py
@@ -14,19 +14,25 @@ def guess2():
             hint=input("Za dużo?(tak,nie): ")
             if hint=="tak":
                 max=guess
+                print("guess={},max={},min={}".format(guess,max,min))
                 i+=1
             elif hint=="nie":
                 hint = input("Za mało?(tak,nie): ")
                 if hint=="tak":
                     min=guess
+                    print("guess={},max={},min={}".format(guess, max, min))
                     i+=1
                 elif hint=="nie":
                     print("Nie oszukuj")
+                    print("guess={},max={},min={}".format(guess, max, min))
                     continue
             else:
                 print("Podaj poprawną odpowiedź tak/nie:")
         else:
             print("Podaj poprawną odpowiedź tak/nie:")
-        guess = int((max - min) / 2) + min
+        if guess==min and max-min==1:
+            guess=guess+1
+        else:
+            guess = int((max - min) / 2) + min
 
 guess2()
